c5cdae5bce77037111877de11cb37763fb988df0
WaldemarChorow <chorow.w@gmail.com>
2026-03-02 15:40:50 +0100

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -3,50 +3,62 @@ let currentImageIndex = 0;
 let images = [
     {
         src: "img/gallery/Component 6.svg",
+        srcLarge: "img/gallery/gallery_img_large/anime-8788959_1280.jpg",
         altText: "picture: city by night"
     },
     {
         src: "img/gallery/Component 7.svg",
+        srcLarge: "img/gallery/gallery_img_large/atmosphere-8752835_1280.png",
         altText: "picture from clouds dark and light"
     },
     {
         src: "img/gallery/Component 8.svg",
+        srcLarge: "img/gallery/gallery_img_large/blue-tit-8521052_1280.jpg",
         altText: "picture from a bird um the tree"
     },
     {
         src: "img/gallery/Component 9.svg",
+        srcLarge: "img/gallery/gallery_img_large/hurricane-92968_1280.jpg",
         altText: "pictur from builing from a twister from the space"
     },
     {
         src: "img/gallery/Component 10.svg",
+        srcLarge: "img/gallery/gallery_img_large/lake-2896379_1280.jpg",
         altText: "picture from a river and mountains in winter "
     },
     {
         src: "img/gallery/Component 11.svg",
+        srcLarge: "img/gallery/gallery_img_large/moorente-8783210_1280.jpg",
         altText: "picture from a duck in water that start to fly"
     },
     {
         src: "img/gallery/Component 12.svg",
+        srcLarge: "img/gallery/gallery_img_large/sea-2563389_1280.jpg",
         altText: "picture from a men in a rock in water by night with a flashlight"
     },
     {
         src: "img/gallery/Component 13.svg",
+        srcLarge: "img/gallery/gallery_img_large/lake-2896379_1280.jpg",
         altText: "picture from a bird of stones"
     },
     {
         src: "img/gallery/Component 14.svg",
+        srcLarge: "img/gallery/gallery_img_large/snow-leopard-cubs-8039138_1280.jpg",
         altText: "picture from two leoprd kitten of a stone"
     },
     {
         src: "img/gallery/Component 15.svg",
+        srcLarge: "img/gallery/gallery_img_large/travel-8785493_1280.jpg",
         altText: "picture from mountains peaks "
     },
     {
         src: "img/gallery/Component 16.svg",
+        srcLarge: "img/gallery/gallery_img_large/winter-1675197_1280.jpg",
         altText: "picture from cfrozen single tree"
     },
         {
         src: "img/gallery/Component 17.svg",
+        srcLarge: "img/gallery/gallery_img_large/Component 17.svg",
         altText: "picture from frozen lake with mountains with snow-capped peaks "
     },
 
@@ -67,20 +79,17 @@ function render(){
 }
 
 function openOverlay(index) {
-    currentImageIndex = index; // Wir merken uns die Nummer des angeklickten Bildes
+    currentImageIndex = index;
     
-    // 1. Hole den Pfad des angeklickten Bildes aus deinem Array
-    let imagePath = images[currentImageIndex].src;
-    
-    // 2. Setze diesen Pfad in das große Bild im Overlay ein
-    document.getElementById('overlay-img').src = imagePath;
-    
-    // 3. Mache das Overlay sichtbar, indem wir die Klasse 'd-none' entfernen
+    let imagePath = images[currentImageIndex].srcLarge;
+
+    document.getElementById('overlay-img').src = imagePath;    
     document.getElementById('overlay').classList.remove('d-none');
 }
 
-// Diese Funktion wird aufgerufen, wenn du auf das 'X' klickst
+// X EXIT Click
 function closeOverlay() {
-    // Verstecke das Overlay wieder, indem wir 'd-none' hinzufügen
+    
     document.getElementById('overlay').classList.add('d-none');
 }
+
